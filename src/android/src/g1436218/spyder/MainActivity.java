a2ce817fa6b3229c79008ddd99a23216d0a920cf
Joey Chan <yam.chan12@imperial.ac.uk>
2014-10-25 15:09:53 +0100

    Application can now detect bluetooth device around

--- src/android/src/g1436218/spyder/MainActivity.java
+++ src/android/src/g1436218/spyder/MainActivity.java
@@ -1,20 +1,55 @@
 package g1436218.spyder;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	
+	private BluetoothAdapter BTAdapter = BluetoothAdapter.getDefaultAdapter();
 
+	private final BroadcastReceiver receiver = new BroadcastReceiver(){
+		@Override
+		public void onReceive(Context context, Intent intent) {
+	            String action = intent.getAction();
+	            if(BluetoothDevice.ACTION_FOUND.equals(action)) {
+	                int rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI,Short.MIN_VALUE);
+	                String name = intent.getStringExtra(BluetoothDevice.EXTRA_NAME);
+	                TextView rssi_msg = (TextView) findViewById(R.id.textView1);
+	                rssi_msg.setText(rssi_msg.getText() + name + " => " + rssi + "dBm\n");
+	            }
+	        }
+	    };
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		registerReceiver(receiver, new IntentFilter(BluetoothDevice.ACTION_FOUND));
+
+		Button boton = (Button) findViewById(R.id.button1);
+		if (!BTAdapter.isEnabled()) {
+			boton.setText("Bluetooth is not enabled");
+		}
+        boton.setOnClickListener(new OnClickListener(){
+            public void onClick(View v) {
+                BTAdapter.startDiscovery();
+            }
+        });
 	}
 
 	@Override
