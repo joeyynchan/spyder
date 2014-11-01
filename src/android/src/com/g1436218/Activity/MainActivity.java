74535e6bae6b7fbc3b8240ffd566681065d60ca3
Joey Chan <yam.chan12@imperial.ac.uk>
2014-11-01 22:28:45 +0000

    implemented background bluetooth task

--- src/android/src/com/g1436218/Activity/MainActivity.java
+++ src/android/src/com/g1436218/Activity/MainActivity.java
@@ -1,98 +1,26 @@
 package com.g1436218.Activity;
 
+import com.g1436218.Object.BackgroundDiscovery;
+
 import g1436218.spyder.R;
-
-import java.util.HashSet;
-
 import android.app.Activity;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
-
-import com.g1436218.Object.Connection;
 
 public class MainActivity extends Activity {
-	
-	private final int TASK_DELAY_DURATION = 15; /* in seconds */
-	private final String TAG = "MainActivity";
-	
-	private BluetoothAdapter BTAdapter = BluetoothAdapter.getDefaultAdapter();
-	private HashSet<Connection> connections;
-	private Handler handler = new Handler();
-	
-	private final BroadcastReceiver receiver = new BroadcastReceiver(){
-		@Override
-		public void onReceive(Context context, Intent intent) {
-
-        	TextView textview = (TextView) findViewById(R.id.textView1);
-        	String action = intent.getAction();
-	            
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                int rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI, Short.MIN_VALUE);
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                connections.add(new Connection(device.getAddress(), rssi));
-                Log.d(TAG, "Device, " + device.getName() + " (" + device.getAddress() + ") has been detected with rssi: " + rssi + " dBm.");
-            } else if (BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)) {
-	            Log.d(TAG, "ACTION_DISCOVERY_STARTED");
-            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
-	            textview.setText(connections.toString() + "\n");
-	            Log.d(TAG, "ACTION_DISCOVERY_FINISHED");
-	            Log.d(TAG, connections.toString());
-            }
-        }
-    };
-    
-    private Runnable mDiscoveryTask = new Runnable() {
-		@Override
-		public void run() {
-			/* Reset the list of connections */
-        	connections = new HashSet<Connection>();
-            BTAdapter.startDiscovery();
-            handler.postDelayed(this, TASK_DELAY_DURATION * 1000);
-		}
-    };
-    
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		/* Enable Bluetooth */
-		if (!BTAdapter.isEnabled()) {
-			BTAdapter.enable();
-		}
-		
-		initializeIntentFilter();
-
-    	handler.post(mDiscoveryTask);
-    	Log.d(TAG, "Handler has posted mDiscoveryTask");
-        
+		BackgroundDiscovery.getInstance(this).execute();
 	}
 	
-	private void initializeIntentFilter() {
-		IntentFilter filter = new IntentFilter();
-		filter.addAction(BluetoothDevice.ACTION_FOUND);
-		filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
-		filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
-		this.registerReceiver(receiver, filter);
-	}
-
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
