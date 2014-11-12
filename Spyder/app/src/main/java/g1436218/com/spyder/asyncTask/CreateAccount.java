831e983b0dd186a1079d14cb978e0a99c2b0aaf3
Cherie Pun <ccp212@ic.ac.uk>
2014-11-12 22:12:25 +0000

    Implemented simple actions when user try to login, redirect to MainActivity through asyncTask LinkDevice. api call has not been included yet

---
+++ Spyder/app/src/main/java/g1436218/com/spyder/asyncTask/CreateAccount.java
@@ -0,0 +1,18 @@
+package g1436218.com.spyder.asyncTask;
+
+import android.app.Activity;
+
+/**
+ * Created by Cherie on 11/9/2014.
+ */
+public class CreateAccount extends BaseAsyncTask {
+
+    protected CreateAccount(Activity activity) {
+        super(activity);
+    }
+
+    @Override
+    protected Void doInBackground(Void... params) {
+        return null;
+    }
+}
