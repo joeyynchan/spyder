dc90ca34b9fa02369f0c2afd9fd95123b1b9d384
Joey Chan <yam.chan12@imperial.ac.uk>
2014-11-20 21:10:49 +0000

    Updated login_activity UI

--- Spyder/app/src/main/java/g1436218/com/spyder/activity/LoginActivity.java
+++ Spyder/app/src/main/java/g1436218/com/spyder/activity/LoginActivity.java
@@ -31,12 +31,12 @@ public class LoginActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.customOnCreate(savedInstanceState, R.layout.activity_login);
+        getActionBar().hide();
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_login, menu);
         return true;
     }
