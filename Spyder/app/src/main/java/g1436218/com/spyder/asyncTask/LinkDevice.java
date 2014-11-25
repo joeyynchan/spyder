b33d0570f2f203caedb282bb7c5394d0ab8719af
Cherie Pun <ccp212@ic.ac.uk>
2014-11-25 01:21:50 +0000

    Added switch statement in LinkDevice to handle different error code returned after calling Login API

--- Spyder/app/src/main/java/g1436218/com/spyder/asyncTask/LinkDevice.java
+++ Spyder/app/src/main/java/g1436218/com/spyder/asyncTask/LinkDevice.java
@@ -38,39 +38,66 @@ public class LinkDevice extends BaseAsyncTask{
         addToParams("mac_address", getDefaultAdapter().getAddress());
 
         JSONObject jsonObject = getJSONFromUrl(URL);
-        try {
-            Log.d(TAG, jsonObject.toString());
-            linked = jsonObject.getString("res").equals("true");
-        } catch (JSONException e) {
-            Log.e(TAG, "Couldn't get result from JSONObject");
-        }
+        Log.d(TAG, this.statusCode + "");
 
-        /* todo: decode the data received from api call
-         * 3 cases
-         * 1) logged in successfully (either already in system/create new link by db
-         * 2) prompt user to unlink previous device and login with this device
-         * 3) account not created, prompt user to register
-         */
-
-        //linked = true;
         return null;
     }
 
     //Start MainActivity if login succeed
     @Override
     public void onPostExecute(Void v) {
-        if (linked) {
         Log.d(TAG, "onPostExecute");
+        setStatusCode(404);
+        switch(this.statusCode) {
+            case (200):{
+                gotoMainActivity();
+                setErrMsgToNotFound();}
+                break;
 
-            Intent intent = new Intent(activity, MainActivity.class);
-            activity.startActivity(intent);
-        } else {
-            //display error message
-            TextView login_text_errmsg = (TextView) activity.findViewById(R.id.login_text_errmsg);
-            login_text_errmsg.setText("Login failed\n");
+            case (201):{
+                gotoMainActivity();
+                setErrMsgToNotFound();}
+                break;
+
+            case (404):
+                setErrMsgToNotFound();
+                break;
+
+            case (409):
+                setErrMsgToNotFound();
+                break;
+
+            default:
+                setErrMsgToNotFound();
+                break;
+        }
+    }
+
+    private void gotoMainActivity(){
+        Intent intent = new Intent(activity, MainActivity.class);
+        activity.startActivity(intent);
+    }
+
+    private void setErrMsgToNotFound(){
+        TextView login_text_errmsg = (TextView) activity.findViewById(R.id.login_text_errmsg);
+
+        switch(this.statusCode) {
+            case(200): login_text_errmsg.setText("Login successfully\n");
+                break;
+            case(201): login_text_errmsg.setText("Successfull linked device\n");
+                break;
+            case(404): login_text_errmsg.setText("Username/Password not match\n");
+                break;
+            case(409): login_text_errmsg.setText("User is already linked with other device\n");
+                break;
+            default:
+                break;
 
         }
+    }
 
+    public void setStatusCode(int statusCode){
+        this.statusCode = statusCode;
     }
 }
 
