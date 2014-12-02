3d36c4f10d78f05de037ff04d85bbc08d1b9e639
Cherie Pun <ccp212@ic.ac.uk>
2014-12-02 03:24:06 +0000

    Successfully fetch profile data from api and display in user fragment, empty string is set if profile does not exist

--- Spyder/app/src/main/java/g1436218/com/spyder/asyncTask/FetchUserProfile.java
+++ Spyder/app/src/main/java/g1436218/com/spyder/asyncTask/FetchUserProfile.java
@@ -1,7 +1,9 @@
 package g1436218.com.spyder.asyncTask;
 
 import android.app.FragmentTransaction;
+import android.util.Log;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import g1436218.com.spyder.R;
@@ -14,6 +16,7 @@ public class FetchUserProfile extends BaseMainAsyncTask {
 
 
     private static String URL = GlobalConfiguration.DEFAULT_URL + "user/profile?user_id=";
+    private static String TAG = "FetchUserProfile";
 
     private String username;
     private User user;
@@ -22,6 +25,7 @@ public class FetchUserProfile extends BaseMainAsyncTask {
         super(activity);
         this.username = username;
         this.URL += username;
+        Log.d(TAG, username);
     }
 
     @Override
@@ -43,7 +47,14 @@ public class FetchUserProfile extends BaseMainAsyncTask {
 
     @Override
     protected Void doInBackgroundOnline(Void... params) {
-        //TODO
+        JSONObject jsonObject = getJSONFromUrl(URL, Responses.GET);
+
+        if(statusCode == 204){
+            //notFound
+        }else if(statusCode == 200){
+            Log.d(TAG, jsonObject.toString());
+            insertDataToUser(jsonObject);
+        }
         return null;
     }
 
@@ -54,5 +65,30 @@ public class FetchUserProfile extends BaseMainAsyncTask {
         fragmentTransaction.replace(R.id.fragment_container, profileFragment, "CURRENT_FRAGMENT");
         fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
+    }
+
+    private void insertDataToUser(JSONObject jsonObject){
+        try {
+            user.setCompany(jsonObject.getString("company"));
+            user.setEmail(jsonObject.getString("email"));
+            user.setExternal_link(jsonObject.getString("external_link"));
+            user.setGender(jsonObject.getString("gender"));
+            user.setName(jsonObject.getString("name"));
+            user.setOccupation(jsonObject.getString("job"));
+            user.setPhone(jsonObject.getString("phone"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /* set texts to empty string in case of profile not found */
+    private void defaultDataToUser(){
+        user.setCompany("");
+        user.setEmail("");
+        user.setExternal_link("");
+        user.setGender("");
+        user.setName("");
+        user.setOccupation("");
+        user.setPhone("");
     }
 }
---
+++ Spyder/app/src/main/java/g1436218/com/spyder/fragment/UserFragment.java
@@ -0,0 +1,50 @@
+package g1436218.com.spyder.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import g1436218.com.spyder.R;
+import g1436218.com.spyder.activity.MainActivity;
+import g1436218.com.spyder.object.User;
+
+public class UserFragment extends BaseMainFragment {
+
+    private User user;
+
+    TextView textview_fragment_user_name ;
+    TextView textview_fragment_user_gender ;
+    TextView textview_fragment_user_occupation;
+    TextView textview_fragment_user_company;
+    TextView textview_fragment_user_phone;
+    TextView textview_fragment_user_email;
+    TextView textview_fragment_user_external_link;
+
+    public UserFragment(MainActivity activity, User user) {
+        super(activity, R.layout.fragment_user);
+        this.user = user;
+    }
+
+    @Override
+    protected void initializeView() {
+        textview_fragment_user_name = (TextView) activity.findViewById(R.id.textview_fragment_user_name);
+        textview_fragment_user_gender = (TextView) activity.findViewById(R.id.textview_fragment_user_gender);
+        textview_fragment_user_occupation = (TextView) activity.findViewById(R.id.textview_fragment_user_occupation);
+        textview_fragment_user_company = (TextView) activity.findViewById(R.id.textview_fragment_user_company);
+        textview_fragment_user_email = (TextView) activity.findViewById(R.id.textview_fragment_user_email);
+        textview_fragment_user_phone = (TextView) activity.findViewById(R.id.textview_fragment_user_phone);
+        textview_fragment_user_external_link = (TextView) activity.findViewById(R.id.textview_fragment_user_external_link);
+
+        textview_fragment_user_name.setText(user.getName());
+        textview_fragment_user_gender.setText(user.getGender());
+        textview_fragment_user_occupation.setText(user.getOccupation());
+        textview_fragment_user_company.setText(user.getCompany());
+        textview_fragment_user_email.setText(user.getEmail());
+        textview_fragment_user_phone.setText(user.getPhone());
+        textview_fragment_user_external_link.setText(user.getExternal_link());
+    }
+
+}
